4b9ff44a609a1c3413760aeb2670de1d40e37c20
Angga <anggaaf007@gmail.com>
2024-10-01 17:01:52 +0700

    Add project files.

---
+++ ToDoListApp/ManageTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListApp
+{
+    public class ManageTask
+    {
+        private List<ModelTask> taskManager;
+
+        public ManageTask()
+        {
+           taskManager = new List<ModelTask>();
+        }
+
+        public void AddTask(string judul, bool isTaskCompleted)
+        {
+            var taskBaru = new ModelTask(judul, isTaskCompleted);
+            taskManager.Add(taskBaru);
+        }
+
+        public void DisplayTask()
+        {
+            foreach (var item in taskManager)
+            {
+                Console.WriteLine($"{item.Id}. {item.Judul} selesai?{item.IsTaskCompleted}");
+            }
+        }
+    }
+}
---
+++ ToDoListApp/ModelTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListApp
+{
+    public class ModelTask
+    {
+        private int _countId = 0;
+        public ModelTask(string judul, bool isTaskCompleted)
+        {
+            if (String.IsNullOrWhiteSpace(judul))
+            {
+                throw new ArgumentException("Judul Kosong", nameof(judul));
+            }
+
+            _countId++;
+            Id = _countId;
+            Judul = judul;
+            IsTaskCompleted = isTaskCompleted;
+        }
+
+        public int Id { get; private set; }
+        public string Judul { get; set; }
+        public bool IsTaskCompleted { get; set; }
+
+    }
+}
