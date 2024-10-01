9bb73bf424abcbb0d2217055730ac1d5d215304e
Angga <anggaaf007@gmail.com>
2024-10-01 17:07:34 +0700

    adding New Class and method with example task

---
+++ ToDoListApp/ToDoApp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListApp
+{
+    public class ToDoApp
+    {
+        private ManageTask _manage;
+
+        public ToDoApp()
+        {
+            _manage = new ManageTask();
+        }
+
+        public void Execute()
+        {
+            _manage.AddTask("Belajar B.Ingris", false);
+            _manage.AddTask("Test 2", true);
+
+            _manage.DisplayTask();
+        }
+    }
+}
